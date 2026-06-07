05d98b5aa310c32b7cb35f93bf91527e8ef24e6a
parth25366-creator <parth25366@iiitd.ac.in>
2026-06-07 11:28:00 +0530

    feat: protect /api/tasks routes with auth middleware

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
+const authMiddleware = require('./middleware/auth');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks', authMiddleware, taskRoutes); // protected
 
 app.get('/', (req, res) => {
   res.json({ message: 'Study Planner API is running!' });
