247be45df8075ca73ddb489e1792e69ec7df9f6c
parth25366-creator <parth25366@iiitd.ac.in>
2026-06-09 12:02:22 +0530

    feat: GET /tasks queries all tasks for logged in user from DB

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -6,8 +6,11 @@ const pool = require('../db');
 router.get('/', async (req, res) => {
   try {
     const userId = req.user.userId;
-    // TODO: query DB
-    res.status(200).json({ message: 'GET tasks hit', userId });
+    const result = await pool.query(
+      'SELECT * FROM tasks WHERE user_id = $1 ORDER BY created_at DESC',
+      [userId]
+    );
+    res.status(200).json(result.rows);
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ error: 'Server error' });
