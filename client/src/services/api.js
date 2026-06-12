61593192234eced512739abd64f72fab8633a937
parth25366-creator <parth25366@iiitd.ac.in>
2026-06-12 11:15:20 +0530

    feat: create axios API instance with base URL

---
+++ client/src/services/api.js
@@ -0,0 +1,17 @@
+// central axios instance for all API calls
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+// attach JWT token to every request automatically
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default API;
