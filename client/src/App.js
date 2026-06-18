e005901706127fd27f0a3c8cc4513fa129ee2211
parth25366-creator <parth25366@iiitd.ac.in>
2026-06-18 11:57:57 +0530

    feat: add PrivateRoute to redirect unauthenticated users to login

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,20 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Tasks from './pages/Tasks';
 
+// protect routes that need auth
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/login" />;
+};
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/tasks" element={<PrivateRoute><Tasks /></PrivateRoute>} />
       </Routes>
     </Router>
   );
